2f0bf7d8081e89f2098fea153ea237a27df7995f
Epsilekt <tim.maes91@gmail.com>
2019-04-03 20:26:28 +0200

    License and icon

--- TTExecuter/TTExecuterPackage.cs
+++ TTExecuter/TTExecuterPackage.cs
@@ -13,23 +13,6 @@ using Task = System.Threading.Tasks.Task;
 
 namespace TTExecuter
 {
-    /// <summary>
-    /// This is the class that implements the package exposed by this assembly.
-    /// </summary>
-    /// <remarks>
-    /// <para>
-    /// The minimum requirement for a class to be considered a valid package for Visual Studio
-    /// is to implement the IVsPackage interface and register itself with the shell.
-    /// This package uses the helper classes defined inside the Managed Package Framework (MPF)
-    /// to do it: it derives from the Package class that provides the implementation of the
-    /// IVsPackage interface and uses the registration attributes defined in the framework to
-    /// register itself and its components with the shell. These attributes tell the pkgdef creation
-    /// utility what data to put into .pkgdef file.
-    /// </para>
-    /// <para>
-    /// To get loaded into VS, the package must be referred by &lt;Asset Type="Microsoft.VisualStudio.VsPackage" ...&gt; in .vsixmanifest file.
-    /// </para>
-    /// </remarks>
     [PackageRegistration(UseManagedResourcesOnly = true, AllowsBackgroundLoading = true)]
     [InstalledProductRegistration("#110", "#112", "1.0", IconResourceID = 400)] // Info on this package for Help/About
     [Guid(TTExecuterPackage.PackageGuidString)]
@@ -70,6 +53,7 @@ namespace TTExecuter
         /// <returns>A task representing the async work of package initialization, or an already completed task if there is none. Do not return null from this method.</returns>
         protected override async Task InitializeAsync(CancellationToken cancellationToken, IProgress<ServiceProgressData> progress)
         {
+            await JoinableTaskFactory.SwitchToMainThreadAsync(cancellationToken);
             base.Initialize();
 
             _dte = await GetServiceAsync(typeof(SDTE)) as DTE;
@@ -84,15 +68,9 @@ namespace TTExecuter
             ThreadHelper.ThrowIfNotOnUIThread();
             _buildEvents = _dte.Events.BuildEvents;
             _buildEvents.OnBuildBegin += OnBuildBegin;
-            _buildEvents.OnBuildDone += OnBuildDone;
         }
 
         private void OnBuildBegin(vsBuildScope scope, vsBuildAction action)
-        {
-            ExecuteTemplates(scope);
-        }
-
-        private void OnBuildDone(vsBuildScope scope, vsBuildAction action)
         {
             ExecuteTemplates(scope);
         }
